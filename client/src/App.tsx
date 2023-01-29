eb17bc287d052e089508ae28b980a9584ddf56fa
Alon Krasne <alonkrasne@gmail.com>
2023-01-29 14:00:58 +0200

    🧹 cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Submissions from "./pages/Submissions";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewSubmission from "./pages/NewSubmission";
-import BindSubmission, { SubmissionData } from "./pages/BindSubmission";
-import { useState } from "react";
-
-const testData: SubmissionData = {
-  id: "123",
-  companyName: "Any comp",
-  address: "Hi there, cal, usa",
-  annualRevenues: "$1,000",
-};
+import BindSubmission from "./pages/BindSubmission";
 
 function App() {
-  const [data, setData] = useState<SubmissionData>({
-    id: "",
-    companyName: "",
-    address: "",
-    annualRevenues: "",
-  });
-
   return (
     <Routes>
       <Route path="/" element={<Submissions />} />
